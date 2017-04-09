3836a939f08ac0a8dc4270a12814f46114f3a021
Aliaksandr Bazhanau <a.bazhanau@adform.com>
2017-04-09 13:48:15 +0300

    controllers

---
+++ VROrienteering/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VROrienteering.Data;
+using VROrienteering.Models;
+
+namespace VROrienteering.Controllers
+{
+    public class ApplicationUsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUsersController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: ApplicationUsers
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.AppUsers.Include(a => a.Group);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: ApplicationUsers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _context.AppUsers
+                .Include(a => a.Group)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Create
+        public IActionResult Create()
+        {
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id");
+            return View();
+        }
+
+        // POST: ApplicationUsers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GroupId,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(applicationUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id", applicationUser.GroupId);
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _context.AppUsers.SingleOrDefaultAsync(m => m.Id == id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id", applicationUser.GroupId);
+            return View(applicationUser);
+        }
+
+        // POST: ApplicationUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("GroupId,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] ApplicationUser applicationUser)
+        {
+            if (id != applicationUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(applicationUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationUserExists(applicationUser.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id", applicationUser.GroupId);
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationUser = await _context.AppUsers
+                .Include(a => a.Group)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(applicationUser);
+        }
+
+        // POST: ApplicationUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var applicationUser = await _context.AppUsers.SingleOrDefaultAsync(m => m.Id == id);
+            _context.AppUsers.Remove(applicationUser);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool ApplicationUserExists(string id)
+        {
+            return _context.AppUsers.Any(e => e.Id == id);
+        }
+    }
+}
